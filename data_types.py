ea48765a223a5a89dc6d0359fb2c0eb2bb305a77
lemingay <leminkay@gmail.com>
2023-08-29 13:23:15 +0400

    add address, bytesarray, some fixes

--- data_types.py
+++ data_types.py
@@ -3,3 +3,5 @@ from enum import Enum
 class Type(Enum):
     INT = 0
     BOOL = 1
+    ADDRESS = 2
+    BYTEARRAY = 3
--- utils.py
+++ utils.py
@@ -20,9 +20,47 @@ def get_random_token(type: Type):
     if type == Type.INT:
         # returns only positive values, not very good for ints. Maybe return hex ?
         return random.randint(0, 2**256 - 1)
+    elif type == Type.ADDRESS:
+        rval = fill_address(hex(random.randint(0, 2**160 - 1)))
+        return checksum_encode(rval)
+    elif type == Type.BYTEARRAY:
+        rval = random.randint(0, 2**256 - 1)
+        return f"b\"{hex(rval)}\""
     elif type == Type.BOOL:
         return bool(random.getrandbits(1))
 
 
 def get_random_element(arr):
     return random.choice(arr)
+
+# https://github.com/vyperlang/vyper/blob/158099b9c1a49b5472293c1fb7a4baf3cd015eb5/vyper/utils.py#L44C1-L51C67
+try:
+    from Crypto.Hash import keccak  # type: ignore
+
+    keccak256 = lambda x: keccak.new(digest_bits=256, data=x).digest()  # noqa: E731
+except ImportError:
+    import sha3 as _sha3
+
+    keccak256 = lambda x: _sha3.sha3_256(x).digest()  # noqa: E731
+
+def fill_address(adr):
+    if len(adr) < 42:
+        adr += "0" * (42 - len(adr))
+    return adr
+
+def bytes_to_int(bytez):
+    o = 0
+    for b in bytez:
+        o = o * 256 + b
+    return o
+
+def checksum_encode(addr):  # Expects an input of the form 0x<40 hex chars>
+    assert addr[:2] == "0x" and len(addr) == 42, addr
+    o = ""
+    v = bytes_to_int(keccak256(addr[2:].lower().encode("utf-8")))
+    for i, c in enumerate(addr[2:]):
+        if c in "0123456789":
+            o += c
+        else:
+            o += c.upper() if (v & (2 ** (255 - 4 * i))) else c.lower()
+    return "0x" + o
