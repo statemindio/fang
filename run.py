655fc2db7a809b5b1c4141e89a3cfaa93f77df8f
ssi91 <sergeev.s.i1991@gmail.com>
2023-11-29 18:30:13 -0400

    add the entry point

---
+++ run.py
@@ -0,0 +1,27 @@
+import atheris
+import atheris_libprotobuf_mutator
+import sys
+
+with atheris.instrument_imports():
+    import sys
+    from vyper import compile_code
+    from vyper.exceptions import CompilerPanic
+
+import vyperProto_pb2
+from converters import ProtoConverter
+
+
+@atheris.instrument_func
+def TestOneProtoInput(msg):
+    proto = ProtoConverter(msg)
+    proto.visit()
+    print(proto.result)
+    print('proto:')
+    print(msg)
+    print("-------------")
+
+
+if __name__ == '__main__':
+    atheris_libprotobuf_mutator.Setup(
+        [sys.argv[0]], TestOneProtoInput, proto=vyperProto_pb2.Contract)
+    atheris.Fuzz()
