dde71d052fd70122ed0e081e9c2f57acda74040f
ssi91 <sergeev.s.i1991@gmail.com>
2023-11-30 15:04:30 -0400

    extract and pass the current length

--- utils.py
+++ utils.py
@@ -23,8 +23,8 @@ def get_nearest_multiple(num, mul):
 
 def get_random_token(type: Type, length=None):
     if type == Type.INT:
-        
-        return random.randint(0, 2**256 - 1), "uint256"
+        length = 256 if length is None else length
+        return random.randint(0, 2**length - 1), f"uint{length}"
     elif type == Type.ADDRESS:
 
         rval = fill_address(hex(random.randint(0, 2**160 - 1)))
@@ -225,3 +225,25 @@ def check_type_requirements(result, current_type, vyper_type, needed_types, leng
     result = str(result)
 
     return result, current_type, vyper_type, True
+
+
+def extract_type_length(current_type, vyper_type):
+    if current_type not in (Type.INT, Type.BytesM):
+        return None
+
+    def _extract_int_length(_vyper_type):
+        split = _vyper_type.split("int")
+        return int(split[1])
+
+    def _extract_bytes_length(_vyper_type):
+        print(_vyper_type)
+        split = _vyper_type.split("bytes")
+        return int(split[1])
+
+    extractor_map = {
+        Type.INT: _extract_int_length,
+        Type.BytesM: _extract_bytes_length
+    }
+
+    type_length = extractor_map[current_type](vyper_type)
+    return type_length
