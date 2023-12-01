ecbb52ae17ecc27c5e194d70d820246d9710b904
ssi91 <sergeev.s.i1991@gmail.com>
2023-12-01 11:45:53 -0400

    add pytest to the dependecies and implement first test-cases

---
+++ tests/test_types.py
@@ -0,0 +1,18 @@
+from types_d import BytesM, String
+
+
+def test_bytes_m_type():
+    bt = BytesM()
+    assert bt.m == 32
+    assert bt.vyper_type == "bytes32"
+
+
+def test_bytes_m_eq():
+    bt = BytesM(8)
+    bt1 = BytesM(8)
+    bt2 = BytesM(9)
+    st = String(8)
+
+    assert bt == bt1
+    assert bt != bt2
+    assert bt != st
