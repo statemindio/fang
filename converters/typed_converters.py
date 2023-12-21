aa4c667c6543c39eb86cb35e3c50ec2558fcf022
ssi91 <sergeev.s.i1991@gmail.com>
2023-12-21 16:55:38 +0400

    switch to passing current_type through stack

--- converters/typed_converters.py
+++ converters/typed_converters.py
@@ -77,7 +77,8 @@ class TypedConverter:
 
         return current_type
 
-    def _visit_var_ref(self, expr, current_type, level=None):
+    def _visit_var_ref(self, expr, level=None):
+        current_type = self.type_stack[len(self.type_stack) - 1]
         allowed_vars = self._var_tracker.get_global_vars(
             current_type
         ) if level is None else self._var_tracker.get_all_allowed_vars(level, current_type)
@@ -85,7 +86,7 @@ class TypedConverter:
         return None if len(allowed_vars) == 0 else random.choice(allowed_vars)
 
     def visit_typed_expression(self, expr, current_type):
-        return self._expression_handlers[current_type.name](expr, current_type)
+        return self._expression_handlers[current_type.name](expr)
 
     def visit_var_decl(self, variable, is_global=False):
         current_type = self.visit_type(variable)
@@ -99,167 +100,191 @@ class TypedConverter:
             self._var_tracker.register_global_variable(var_name, current_type)
         else:
             self._var_tracker.register_function_variable(var_name, self._block_level_count, current_type)
-            value = self.visit_typed_expression(variable.expr, current_type)
+            value = self.visit_typed_expression(variable.expr)
             result += f"{result} = {value}"
+        self.type_stack.pop()
         return result
 
     def visit_func(self, function):
         # TODO: implement
         pass
 
-    def visit_address_expression(self, expr, current_type):
+    def visit_address_expression(self, expr):
         if expr.HasField("cmp"):
-            return self.visit_create_min_proxy(expr.cmp, current_type)
+            return self.visit_create_min_proxy(expr.cmp)
         if expr.HasField("cfb"):
-            return self.visit_create_from_blueprint(expr.cfb, current_type)
+            return self.visit_create_from_blueprint(expr.cfb)
         if expr.HasField("varRef"):
             # TODO: it has to be decided how exactly to track a current block level or if it has to be passed
-            result = self._visit_var_ref(expr.varRef, current_type, self._block_level_count)
+            result = self._visit_var_ref(expr.varRef, self._block_level_count)
             if result is not None:
                 return result
-        return self.create_literal(expr.lit, current_type)
+        return self.create_literal(expr.lit)
 
-    def visit_create_min_proxy(self, cmp, current_type):
-        target = self.visit_address_expression(cmp.target, current_type)
+    def visit_create_min_proxy(self, cmp):
+        target = self.visit_address_expression(cmp.target)
         result = f"create_minimal_proxy_to({target}"
         if cmp.HasField("value"):
-            value = self._visit_int_expression(cmp.value, Int(256))
+            self.type_stack.append(Int(256))
+            value = self._visit_int_expression(cmp.value)
             result = f"{result}, value = {value}"
+            self.type_stack.pop()
         if cmp.HasField("salt"):
-            salt = self._visit_bytes_m_expression(cmp.salt, BytesM(32))
+            self.type_stack.append(Bytes(32))
+            salt = self._visit_bytes_m_expression(cmp.salt)
             result = f"{result}, salt = {salt}"
+            self.type_stack.pop()
         result = f"{result})"
 
         return result
 
-    def visit_create_from_blueprint(self, cfb, current_type):
-        target = self.visit_address_expression(cfb.target, current_type)
+    def visit_create_from_blueprint(self, cfb):
+        target = self.visit_address_expression(cfb.target)
         result = f"create_from_blueprint({target}"
 
         # TODO: args parameter is not handled yet
 
         if cfb.HasField("rawArgs"):
+            self.type_stack.append(Bool())
             raw_args = self._visit_bool_expression(cfb.rawArgs)
             result = f"{result}, raw_args = {raw_args}"
+            self.type_stack.pop()
         if cfb.HasField("value"):
-            value = self._visit_int_expression(cfb.value, Int(256))
+            self.type_stack.append(Int(256))
+            value = self._visit_int_expression(cfb.value)
             result = f"{result}, value = {value}"
+            self.type_stack.pop()
         if cfb.HasField("code_offset"):
-            offset = self._visit_int_expression(cfb.code_offset, Int(256))
+            self.type_stack.append(Int(256))
+            offset = self._visit_int_expression(cfb.code_offset)
             result = f"{result}, code_offset = {offset}"
+            self.type_stack.pop()
         if cfb.HasField("salt"):
-            salt = self._visit_bytes_m_expression(cfb.salt, BytesM(32))
+            self.type_stack.append(Int(256))
+            salt = self._visit_bytes_m_expression(cfb.salt)
             result = f"{result}, salt = {salt}"
+            self.type_stack.pop()
         result = f"{result})"
 
         return result
 
-    def create_literal(self, lit, current_type):
+    def create_literal(self, lit):
+        current_type = self.type_stack[len(self.type_stack) - 1]
         return current_type.generate_literal(getattr(lit, LITERAL_ATTR_MAP[current_type.name]))
 
-    def _visit_bool_expression(self, expr, current_type):
+    def _visit_bool_expression(self, expr):
         if expr.HasField("boolBinOp"):
-            left = self._visit_bool_expression(expr.boolBinOp.left, current_type)
-            right = self._visit_bool_expression(expr.boolBinOp.right, current_type)
+            left = self._visit_bool_expression(expr.boolBinOp.left)
+            right = self._visit_bool_expression(expr.boolBinOp.right)
             bin_op = get_bin_op(expr.boolBinOp.op, BIN_OP_BOOL_MAP)
             result = f"{left} {bin_op} {right}"
             return result
         if expr.HasField("boolUnOp"):
-            operand = self._visit_bool_expression(expr.boolUnOp.expr, current_type)
+            operand = self._visit_bool_expression(expr.boolUnOp.expr)
             result = f"not {operand}"
             return result
         if expr.HasField("intBoolBinOp"):
             # TODO: here probably must be different kinds of Int
-            left = self._visit_int_expression(expr.intBoolBinOp.left, Int())
-            right = self._visit_int_expression(expr.intBoolBinOp.right, Int())
+            self.type_stack.append(Int(256))
+            left = self._visit_int_expression(expr.intBoolBinOp.left)
+            right = self._visit_int_expression(expr.intBoolBinOp.right)
             bin_op = get_bin_op(expr.intBoolBinOp.op, INT_BIN_OP_BOOL_MAP)
             result = f"{left} {bin_op} {right}"
+            self.type_stack.pop()
             return result
         if expr.HasField("decBoolBinOp"):
-            left = self._visit_decimal_expression(expr.decBoolBinOp.left, Decimal())
-            right = self._visit_decimal_expression(expr.decBoolBinOp.right, Decimal())
+            self.type_stack.append(Decimal())
+            left = self._visit_decimal_expression(expr.decBoolBinOp.left)
+            right = self._visit_decimal_expression(expr.decBoolBinOp.right)
             bin_op = get_bin_op(expr.intBoolBinOp.op, INT_BIN_OP_BOOL_MAP)
             result = f"{left} {bin_op} {right}"
+            self.type_stack.pop()
             return result
         if expr.HasField("varRef"):
             # TODO: it has to be decided how exactly to track a current block level or if it has to be passed
-            result = self._visit_var_ref(expr.varRef, current_type, self._block_level_count)
+            result = self._visit_var_ref(expr.varRef, self._block_level_count)
             if result is not None:
                 return result
-        return self.create_literal(expr.lit, current_type)
+        return self.create_literal(expr.lit)
 
-    def _visit_int_expression(self, expr, current_type):
+    def _visit_int_expression(self, expr):
         if expr.HasField("binOp"):
-            left = self._visit_int_expression(expr.binOp.left, current_type)
-            right = self._visit_int_expression(expr.binOp.right, current_type)
+            left = self._visit_int_expression(expr.binOp.left)
+            right = self._visit_int_expression(expr.binOp.right)
             bin_op = get_bin_op(expr.binOp.op, BIN_OP_MAP)
             result = f"{left} {bin_op} {right}"
             return result
         if expr.HasField("unOp"):
-            result = self._visit_int_expression(expr.unOp.expr, current_type)
+            result = self._visit_int_expression(expr.unOp.expr)
             # TODO: implement a stack for arithmetic operations to avoin redundant brackets
             result = f"(-({result}))"
             return result
         if expr.HasField("varRef"):
             # TODO: it has to be decided how exactly to track a current block level or if it has to be passed
-            result = self._visit_var_ref(expr.varRef, current_type, self._block_level_count)
+            result = self._visit_var_ref(expr.varRef, self._block_level_count)
             if result is not None:
                 return result
-        return self.create_literal(expr.lit, current_type)
+        return self.create_literal(expr.lit)
 
-    def _visit_bytes_m_expression(self, expr, current_type):
+    def _visit_bytes_m_expression(self, expr):
         if expr.HasField("sha"):
             # FIXME: length of current BytesM might me less than 32, If so, the result of `sha256` must be converted
-            return self._visit_sha256(expr.sha, current_type)
+            return self._visit_sha256(expr.sha)
         if expr.HasField("varRef"):
             # TODO: it has to be decided how exactly to track a current block level or if it has to be passed
-            result = self._visit_var_ref(expr.varRef, current_type, self._block_level_count)
+            result = self._visit_var_ref(expr.varRef, self._block_level_count)
             if result is not None:
                 return result
-        return self.create_literal(expr.lit, current_type)
+        return self.create_literal(expr.lit)
 
-    def _visit_sha256(self, expr, current_type):
+    def _visit_sha256(self, expr):
         result = "sha256("
         if expr.HasField("strVal"):
-            value = self._visit_string_expression(expr.strVal, String(100))
+            self.type_stack.append(String(100))
+            value = self._visit_string_expression(expr.strVal)
+            self.type_stack.pop()
             return f"{result}{value})"
         if expr.HasField("bVal"):
-            value = self._visit_bytes_expression(expr.bVal, Bytes(100))
+            self.type_stack.append(Bytes(100))
+            value = self._visit_bytes_expression(expr.bVal)
+            self.type_stack.pop()
             return f"{result}{value})"
-        value = self._visit_bytes_m_expression(expr.bmVal, BytesM(32))
+        self.type_stack.append(BytesM(32))
+        value = self._visit_bytes_m_expression(expr.bmVal)
+        self.type_stack.pop()
         return f"{result}{value})"
 
-    def _visit_decimal_expression(self, expr, current_type):
+    def _visit_decimal_expression(self, expr):
         if expr.HasField("binOp"):
-            left = self._visit_decimal_expression(expr.binOp.left, current_type)
-            right = self._visit_decimal_expression(expr.binOp.right, current_type)
+            left = self._visit_decimal_expression(expr.binOp.left)
+            right = self._visit_decimal_expression(expr.binOp.right)
             bin_op = get_bin_op(expr.binOp.op, BIN_OP_MAP)
             result = f"{left} {bin_op} {right}"
             return result
         if expr.HasField("unOp"):
-            result = self._visit_decimal_expression(expr.unOp.expr, current_type)
+            result = self._visit_decimal_expression(expr.unOp.expr)
             # TODO: implement a stack for arithmetic operations to avoin redundant brackets
             result = f"(-({result}))"
             return result
         if expr.HasField("varRef"):
             # TODO: it has to be decided how exactly to track a current block level or if it has to be passed
-            result = self._visit_var_ref(expr.varRef, current_type, self._block_level_count)
+            result = self._visit_var_ref(expr.varRef, self._block_level_count)
             if result is not None:
                 return result
-        return self.create_literal(expr.lit, current_type)
+        return self.create_literal(expr.lit)
 
-    def _visit_bytes_expression(self, expr, current_type):
+    def _visit_bytes_expression(self, expr):
         if expr.HasField("varRef"):
             # TODO: it has to be decided how exactly to track a current block level or if it has to be passed
-            result = self._visit_var_ref(expr.varRef, current_type, self._block_level_count)
+            result = self._visit_var_ref(expr.varRef, self._block_level_count)
             if result is not None:
                 return result
-        return self.create_literal(expr.lit, current_type)
+        return self.create_literal(expr.lit)
 
-    def _visit_string_expression(self, expr, current_type):
+    def _visit_string_expression(self, expr):
         if expr.HasField("varRef"):
             # TODO: it has to be decided how exactly to track a current block level or if it has to be passed
-            result = self._visit_var_ref(expr.varRef, current_type, self._block_level_count)
+            result = self._visit_var_ref(expr.varRef, self._block_level_count)
             if result is not None:
                 return result
-        return self.create_literal(expr.lit, current_type)
+        return self.create_literal(expr.lit)
