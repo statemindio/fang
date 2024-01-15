0e2f67117e5a54a19c6c626320cef3b959ec06ac
ssi91 <sergeev.s.i1991@gmail.com>
2024-01-14 20:11:28 -0400

    add docstrings to the var_tracker

--- var_tracker.py
+++ var_tracker.py
@@ -2,6 +2,10 @@ from types_d.base import BaseType
 
 
 class VarTracker:
+    """
+    Manages variables, tracks its ID and provides access to variables up to a certain level of block.
+    """
+
     GLOBAL_KEY = "__global__"
     FUNCTION_KEY = "__function__"
 
@@ -14,14 +18,32 @@ class VarTracker:
         }
 
     def next_id(self, var_type) -> int:
+        """
+
+        :param var_type: type instance
+        :type var_type: BaseType
+        :return: the next ID for a certain `var_type`
+        """
         return self.current_id(var_type) + 1
 
     def current_id(self, var_type) -> int:
+        """
+        Checks if the current ID exists for a certain `var_type` and returns the ID if it exists
+        :param var_type: type instance
+        :type var_type: BaseType
+        :return: the current ID for a certain `var_type`. If it doesn't exist, returns -1
+        """
         if var_type.name not in self._var_id_map:
             self._var_id_map[var_type.name] = -1
         return self._var_id_map[var_type.name]
 
     def register_function_variable(self, name, level, var_type: BaseType):
+        """
+        Sets a new variable to the passed `level`
+        :param name: name of the new variable
+        :param level:
+        :param var_type:
+        """
         if var_type.vyper_type not in self._vars[self.FUNCTION_KEY]:
             self._vars[self.FUNCTION_KEY][var_type.vyper_type] = {
                 level: []
@@ -35,6 +57,11 @@ class VarTracker:
         self._var_id_map[var_type.name] = self.next_id(var_type)
 
     def register_global_variable(self, name, var_type: BaseType):
+        """
+        Sets a new global variable
+        :param name: name of the new variable
+        :param var_type:
+        """
         if var_type.vyper_type not in self._vars[self.GLOBAL_KEY]:
             self._vars[self.GLOBAL_KEY][var_type.vyper_type] = []
         # TODO: check if a variable already exist
@@ -43,6 +70,10 @@ class VarTracker:
         self._var_id_map[var_type.name] = self.next_id(var_type)
 
     def remove_function_level(self, level: int):
+        """
+        Removes the specified level's variables
+        :param level:
+        """
         for vyper_type in self._vars[self.FUNCTION_KEY]:
             if level not in self._vars[self.FUNCTION_KEY][vyper_type]:
                 continue
@@ -50,11 +81,23 @@ class VarTracker:
             self._vars[self.FUNCTION_KEY][vyper_type][level] = []
 
     def get_all_allowed_vars(self, level: int, var_type: BaseType):
+        """
+
+        :param level:
+        :param var_type:
+        :return: list of allowed variables. It's a united of the global variables and function variables
+        allowed on the given level
+        """
         allowed_vars = self.get_global_vars(var_type)
         for i in range(level + 1):
             allowed_vars.extend(self._vars[self.FUNCTION_KEY].get(var_type.vyper_type, {}).get(i, []))
         return allowed_vars
 
     def get_global_vars(self, var_type: BaseType):
+        """
+
+        :param var_type:
+        :return: list of allowed global variables
+        """
         allowed_vars = [f"self.{v}" for v in self._vars[self.GLOBAL_KEY].get(var_type.vyper_type, [])]
         return allowed_vars
