231c4def77f43d1806a72e952d0a1080828592a7
ssi91 <sergeev.s.i1991@gmail.com>
2024-02-07 15:39:17 -0400

    implement function calls indexes

--- converters/function_converter.py
+++ converters/function_converter.py
@@ -1,22 +1,27 @@
 from collections import defaultdict
 from google._upb._message import RepeatedCompositeContainer
 
+
 class FunctionConverter:
     def __init__(self):
         self._call_tree = defaultdict(list)
 
     def _find_func_call(self, i, statement):
+        if isinstance(statement, (int, bool, str)):
+            return
         fields = statement.ListFields()
         if len(fields) == 0:
             return
-        field = fields[0]
-        if isinstance(field, RepeatedCompositeContainer):
-            for f in field:
-                self._find_func_call(i, f)
-        if field.name == "func_call":
-            self._call_tree[i].append(statement.func_call.func_num)
-        else:
-            self._find_func_call(i, field)
+        for field in fields:
+            # TODO: this test can be replaced with checking `field[0].label == field[0].LABEL_REPEATED`
+            if isinstance(field[1], RepeatedCompositeContainer):
+                for f in field[1]:
+                    self._find_func_call(i, f)
+                continue
+            if field[0].name == "func_call":
+                self._call_tree[i].append(statement.func_call.func_num)
+            else:
+                self._find_func_call(i, field[1])
 
     def setup_order(self, functions):
         for i, function in enumerate(functions):
