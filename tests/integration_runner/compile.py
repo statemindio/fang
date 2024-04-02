fda696d7832b4c5f5b9d5c668a729d2d89ef1d5b
ssi91 <sergeev.s.i1991@gmail.com>
2024-04-02 17:58:55 -0400

    add logging to the compiler service currently it just saves resulting bytecode or compilation error if it fails

--- tests/integration_runner/compile.py
+++ tests/integration_runner/compile.py
@@ -2,6 +2,10 @@ import json
 
 import pika
 from vyper import compile_code
+from db import get_mongo_client
+
+db_ = get_mongo_client()
+compilation_results = db_["compilation_results"]
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
 channel = connection.channel()
@@ -14,11 +18,16 @@ channel.queue_declare(queue_name)
 def callback(ch, method, properties, body):
     data = json.loads(body)
     print(data["_id"])
+    gen = {
+        "generation_id": data["_id"]
+    }
     try:
         comp = compile_code(data["result"])
         print(comp)
+        gen.update(comp)
     except Exception as e:
-        pass
+        gen.update({"error": str(e)})
+    compilation_results.insert_one(gen)
 
 
 channel.basic_consume(queue_name, on_message_callback=callback, auto_ack=True)
