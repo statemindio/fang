5e4f0845d6bd6fd2e08a55fe3ab9f9e02c0a2650
ssi91 <sergeev.s.i1991@gmail.com>
2024-07-04 12:17:03 -0400

    concept of the runner

---
+++ runners/simple_runner.py
@@ -0,0 +1,71 @@
+import json
+import pickle
+from collections import defaultdict
+
+from db import get_mongo_client
+
+import boa
+
+sender = ""  # TODO: init actual sender address
+
+
+class ContractsProvider:
+    def __init__(self, db_connector):
+        self._source = db_connector
+
+    def get_contracts(self):
+        contracts = self._source.find({"ran": False})
+        return contracts
+
+
+def get_input_params(types):
+    # TODO: implement
+    return []
+
+
+def external_nonpayable_runner(contract, abi_func):
+    func = getattr(contract, abi_func["name"])
+    input_params = get_input_params(abi_func["inputs"])
+    computation, output = func(*input_params)
+    return computation, output
+
+
+def f(comp, ret) -> dict:
+    # TODO: it's a stub for a function to be supposed to deal with return value and computation results
+    return dict()
+
+
+def save_results(res):
+    # TODO: it's a stub for a function to be supposed to deal with runners' results
+    pass
+
+
+if __name__ == "__main__":
+    collections = (
+        'compilation_results_0_3_10_codesize',
+        'compilation_results_0_3_10_gas',
+    )
+    db_contracts = get_mongo_client()
+    contracts_cols = (db_contracts[col] for col in collections)
+    contracts_providers = (ContractsProvider(contracts_col) for contracts_col in contracts_cols)
+    reference_amount = len(collections)
+    interim_results = defaultdict(list)
+    for provider in contracts_providers:
+        contracts = provider.get_contracts()
+        for contract_desc in contracts:
+            at, _ = boa.env.deploy_code(
+                bytecode=bytes.fromhex(contract_desc["bytecode"][2:])
+            )
+
+            factory = boa.loads_abi(json.dumps(contract_desc["abi"]), name="Foo")
+            contract = factory.at(at)
+            for abi_item in contract_desc["abi"]:
+                if abi_item["type"] == "function" and abi_item["stateMutability"] == "nonpayable":
+                    comp, ret = external_nonpayable_runner(contract, abi_item)
+
+                    # well, now we save some side effects as json since it's not
+                    # easy to pickle an object of abc.TitanoboaComputation
+                    function_call_res = f(comp, ret)
+                    interim_results[contract_desc["_id"]].append(function_call_res)
+    results = dict((_id, res) for _id, res in interim_results.items() if len(res) == reference_amount)
+    save_results(results)
