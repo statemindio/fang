af576372258aa1762a396ff25ae4798aeb0a616b
ssi91 <sergeev.s.i1991@gmail.com>
2024-07-10 00:59:34 -0400

    add a bunch of small fixes

--- runners/simple_runner.py
+++ runners/simple_runner.py
@@ -19,10 +19,11 @@ class ContractsProvider:
     @contextlib.contextmanager
     def get_contracts(self):
         contracts = self._source.find({"ran": False})
+        contracts = list(contracts)
         yield contracts
         self._source.update_many(
             {'_id': {'$in': [c['_id'] for c in contracts]}},
-            {'ran': True}
+            {'$set': {'ran': True}}
         )
 
 
@@ -51,8 +52,28 @@ def compose_result(comp, ret) -> dict:
 
 
 def save_results(res):
-    for gid, results in res.items():
-        run_results_collection.insert_many({"generation_id": gid, "results": results})
+    to_save = [{"generation_id": gid, "results": results} for gid, results in res.items()]
+    if len(to_save) == 0:
+        print("No results to save...")
+        return
+    run_results_collection.insert_many(to_save)
+
+
+def skip_execution_errors(f):
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception:
+            return dict(state=None, memory=None, consumed_gas=None, return_value=None)
+
+    return wrapper
+
+
+@skip_execution_errors
+def execution_result(contract, abi_item):
+    comp, ret = external_nonpayable_runner(contract, abi_item)
+    function_call_res = compose_result(comp, ret)
+    return function_call_res
 
 
 if __name__ == "__main__":
@@ -63,16 +84,19 @@ if __name__ == "__main__":
     run_results_collection = db_contracts["run_results"]
 
     collections = [
-        f"compilation_results_{vyper.__version__}_{c['name']}" for c in conf.compilers
+        f"compilation_results_{vyper.__version__.replace('.', '_')}_{c['name']}" for c in conf.compilers
     ]
+    print("Collections: ", collections, flush=True)
     contracts_cols = (db_contracts[col] for col in collections)
 
+    contracts_providers = [ContractsProvider(contracts_col) for contracts_col in contracts_cols]
+    reference_amount = len(collections)
+
     while True:
-        contracts_providers = (ContractsProvider(contracts_col) for contracts_col in contracts_cols)
-        reference_amount = len(collections)
         interim_results = defaultdict(list)
         for provider in contracts_providers:
             with provider.get_contracts() as contracts:
+                print(f"Amount of contracts: ", len(contracts), flush=True)
                 for contract_desc in contracts:
                     at, _ = boa.env.deploy_code(
                         bytecode=bytes.fromhex(contract_desc["bytecode"][2:])
@@ -80,15 +104,16 @@ if __name__ == "__main__":
 
                     factory = boa.loads_abi(json.dumps(contract_desc["abi"]), name="Foo")
                     contract = factory.at(at)
+                    r = []
                     for abi_item in contract_desc["abi"]:
                         if abi_item["type"] == "function" and abi_item["stateMutability"] == "nonpayable":
-                            comp, ret = external_nonpayable_runner(contract, abi_item)
-
-                            # well, now we save some side effects as json since it's not
-                            # easy to pickle an object of abc.TitanoboaComputation
-                            function_call_res = compose_result(comp, ret)
-                            interim_results[contract_desc["_id"]].append(function_call_res)
+                            function_call_res = execution_result()
+                            r.append(function_call_res)
+                    interim_results[contract_desc["generation_id"]].append(r)
+            print("interim results", interim_results, flush=True)
         results = dict((_id, res) for _id, res in interim_results.items() if len(res) == reference_amount)
+        print("results", results, flush=True)
         save_results(results)
 
+        print("waiting....", flush=True)
         time.sleep(2)  # wait two seconds before the next request
