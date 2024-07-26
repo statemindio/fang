0e09b5830477812b4b90b6e35b1b69073b13c5aa
lemingay <leminkay@gmail.com>
2024-07-26 13:53:27 +0400

    fix test & add json test

--- tests/test_input_generator.py
+++ tests/test_input_generator.py
@@ -6,6 +6,7 @@ import pytest
 from input_generation import InputGenerator, InputStrategy
 from runners.simple_runner import handle_compilation
 from types_d import types as t
+from json_encoders import ExtendedEncoder, ExtendedDecoder
 
 data = [
     ([t.Int()], [84747631942840761409198475171043116002924132430274400095798688737583350222083]),
@@ -57,14 +58,36 @@ def test_handle_compilation():
       }
     ],
     "bytecode": "0x3461004857602061007c5f395f51600160208261007c015f395f511161004857602060208261007c015f395f5101808261007c01604039505061001e61004c60623961001e6062f35b5f80fd5f3560e01c6329b9c7e48118610016573461001a57005b5f5ffd5b5f80fd84181e8000a16576797065728300030a0012",
-    "function_input_types": "800495d8000000000000007d94288c085f5f696e69745f5f945d948c0d74797065735f642e7479706573948c0542797465739493942981947d94288c025f6d944b018c105f76616c75655f67656e657261746f72948c1774797065735f642e76616c75655f67656e657261746f72948c0e427974657352616e646f6d47656e9493942981948c125f6c69746572616c5f67656e657261746f72948c1f74797065735f642e6c69746572616c5f76616c75655f67656e657261746f72948c0f42797465734c69746572616c47656e9493942981947562618c0666756e635f30945d94752e",
     "generation_id": "669ff18aa9c64fcd85e18039",
     "ran": false
   }"""
+    input_types = {'__init__': [t.Bytes(1)], 'func_0': []}
+    input_values = {}
+    for name, types in input_types.items():
+      input_values[name] = igen.generate(types)
+    input_values = json.dumps(input_values, cls=ExtendedEncoder)
 
     compilation_obj = json.loads(compilation)
+    compilation_obj['function_input_values'] = input_values
 
-    compilation_result = handle_compilation(compilation_obj, igen)
+    compilation_result = handle_compilation(compilation_obj)
     assert compilation_result == [
         {'state': ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'], 'memory': '', 'consumed_gas': 48,
          'return_value': 'null'}]
+
+json_data = [
+  ([t.Bytes(1), t.Bytes(10), t.Bytes(32)], 'bytes', lambda x: x.hex()),
+  ([t.Decimal(), t.Decimal(), t.Decimal()], 'Decimal', lambda x: str(x)),
+]
+
+@pytest.mark.parametrize("types, type_name, fn", json_data)
+def test_extended_json(types, type_name, fn):
+    igen = InputGenerator(InputStrategy.DEFAULT)
+
+    generated_value = igen.generate(types)
+
+    for i in range(len(types)):
+        enc = json.dumps(generated_value[i], cls=ExtendedEncoder)
+        assert enc == '{"_type": "%s", "value": "%s"}' % (type_name, fn(generated_value[i]))
+        dec = json.loads(enc, cls=ExtendedDecoder)
+        assert dec == generated_value[i]
