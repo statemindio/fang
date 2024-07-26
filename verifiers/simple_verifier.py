1f4053053e5c9186a6ea7a03d091661f77843da6
ssi91 <sergeev.s.i1991@gmail.com>
2024-07-25 21:46:54 -0400

    common structure of verifiers

--- verifiers/simple_verifier.py
+++ verifiers/simple_verifier.py
@@ -4,9 +4,27 @@ from db import get_mongo_client
 conf = Config()
 
 
-def verify_two_results(_res0, _res1):
-    # TODO: call low lever verifiers here
+def storage_verifier(storage0, storage1):
     pass
+
+
+def memory_verifier(memory0, memory1):
+    pass
+
+
+def gas_verifier(gas0, gas1):
+    pass
+
+
+def return_value_verifier(value0, value1):
+    pass
+
+
+def verify_two_results(_res0, _res1):
+    storage_verifier(_res0["storage"], _res1["storage"])
+    memory_verifier(_res0["memory"], _res1["memory"])
+    gas_verifier(_res0["consumed_gas"], _res1["consumed_gas"])
+    return_value_verifier(_res0["return_value"], _res1["return_value"])
 
 
 def verify_results(_results):
