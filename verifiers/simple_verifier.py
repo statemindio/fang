44a97b60fefc4bee9291c9b1384a3a8a053bafd7
ssi91 <sergeev.s.i1991@gmail.com>
2024-07-29 21:16:15 -0400

    implement a minimal working implementation of verifiers

--- verifiers/simple_verifier.py
+++ verifiers/simple_verifier.py
@@ -1,9 +1,13 @@
 import json
+import logging
+import time
 
 from config import Config
 from db import get_mongo_client
 
-conf = Config()
+# TODO: get level from config
+logger = logging.getLogger("verifier")
+logging.basicConfig(format='%(name)s:%(levelname)s:%(asctime)s:%(message)s', level=logging.DEBUG)
 
 
 class VerifierException(Exception):
@@ -26,42 +30,75 @@ def gas_verifier(gas0, gas1):
 
 
 def return_value_verifier(value0, value1):
-    loaded_value0 = json.loads(value0)
-    loaded_value1 = json.loads(value1)
+    loaded_value0 = value0
+    loaded_value1 = value1
     if loaded_value0 != loaded_value1:
         raise VerifierException(f"Return Value discrepancy: {loaded_value0} | {loaded_value1}")
 
 
 def verify_two_results(_res0, _res1):
-    storage_verifier(_res0["storage"], _res1["storage"])
-    memory_verifier(_res0["memory"], _res1["memory"])
-    gas_verifier(_res0["consumed_gas"], _res1["consumed_gas"])
-    return_value_verifier(_res0["return_value"], _res1["return_value"])
+    verifiers = {
+        "Storage": (storage_verifier, (_res0["state"], _res1["state"])),
+        "Memory": (memory_verifier, (_res0["memory"], _res1["memory"])),
+        "Gas": (gas_verifier, (_res0["consumed_gas"], _res1["consumed_gas"])),
+        "Return_Value": (return_value_verifier, (_res0["return_value"], _res1["return_value"]))
+    }
+    d = {}
+    for name, (verifier, params) in verifiers.items():
+        try:
+            verifier(*params)
+            d[name] = None
+        except VerifierException as e:
+            d[name] = str(e)
+            continue
+    return d
 
 
 def verify_results(_results):
-    compilers = _results["results"].keys()
-    for _res in zip(_results["results"][compiler_key] for compiler_key in compilers):
+    compilers = list(_results["results"].keys())
+
+    func_results = []
+    for _res in zip(*[_results["results"][compiler_key] for compiler_key in compilers]):
+        logger.debug(f"Function result: {_res}")
+
+        # each item of `r` is a list of comparisons of a function
+        r = []
+
         for i, _func_res in enumerate(_res):
-            if i == len(_res):
+            if i == len(_res) - 1:
                 break
-            try:
-                verify_two_results(_func_res, _res[i + 1])
-            except VerifierException as e:
-                # TODO: save discrepancy to db here
-                pass
+            d = verify_two_results(_func_res, _res[i + 1])
+            r.append({
+                "compilers": (compilers[i], compilers[i + 1]),
+                "results": d
+            })
+        func_results.append(r)
+    return func_results
 
 
 if __name__ == '__main__':
+    conf = Config()
     db_client = get_mongo_client(conf.db["host"], conf.db["port"])
     results_collection = db_client["run_results"]
+    verification_results_collection = db_client["verification_results"]
 
-    unhandled_results = list(results_collection.find({"is_handled": False}))
+    while True:
+        unhandled_results = list(results_collection.find({"is_handled": False}))
+        logger.debug(f"Unhandled results received: {unhandled_results}")
 
-    for res in unhandled_results:
-        verify_results(res)
+        verification_results = []
+        for res in unhandled_results:
+            logger.info(f"Handling result: {res['generation_id']}")
+            logger.debug(res)
+            _r = verify_results(res)
+            verification_results.append({"generation_id": res["generation_id"], "results": _r})
 
-    results_collection.update_many(
-        {"_id": {"$in": [r["_id"] for r in unhandled_results]}},
-        {"$set": {"is_handled": True}}
-    )
+        if len(verification_results) != 0:
+            verification_results_collection.insert_many(verification_results)
+
+        if len(unhandled_results) != 0:
+            results_collection.update_many(
+                {"_id": {"$in": [r["_id"] for r in unhandled_results]}},
+                {"$set": {"is_handled": True}}
+            )
+        time.sleep(5)
